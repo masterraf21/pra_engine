c598752870c29d06f36b9298c15bdef3d037c024
Rafi Abbel <masterraf21@gmail.com>
2022-04-07 23:38:49 +0700

    migrate clock skew to pydantic

--- zipkin/clock_skew.py
+++ zipkin/clock_skew.py
@@ -1,8 +1,9 @@
 from .span_node import *
+from .models import *
 
 
 class ClockSkew:
-    def __init__(self, endpoint, skew) -> None:
+    def __init__(self, endpoint: Endpoint, skew) -> None:
         self._endpoint = endpoint
         self._skew = skew
 
@@ -15,34 +16,35 @@ class ClockSkew:
         return self._skew
 
 
-def ips_match(a, b):
+def ips_match(a: Optional[Endpoint], b: Optional[Endpoint]):
     if not a or not b:
         return False
-    if a["ipv6"] and b["ipv6"] and a["ipv6"] == b["ipv6"]:
+    if a.ipv6 and b.ipv6 and a.ipv6 == b.ipv6:
         return True
-    if not a["ipv4"] and not b["ipv4"]:
+    if not a.ipv4 and not b.ipv4:
         return False
 
-    return a["ipv4"] == b["ipv4"]
+    return a.ipv4 == b.ipv4
 
 
-def adjust_timestamps(span, skew):
-    if not ips_match(skew["endpoint"], span["localEndpoint"]):
+def adjust_timestamps(span: Span, skew):
+    if not ips_match(skew.endpoint, span.localEndpoint):
         return span
 
     result = span
-    if span["timestamp"]:
-        result["timestamp"] = span["timestamp"] - skew["skew"]
+    if span.timestamp:
+        result.timestamp = span.timestamp - skew.skew
 
-    annotation_length = len(span["annotations"])
+    annotation_length = len(span.annotations)
     if annotation_length > 0:
-        result["annotations"] = []
+        result.annotations = []
     for i in range(annotation_length):
-        a = span["annotations"][i]
-        result["annotations"][i] = {
-            "timestamp": a["timestamp"] - skew["skew"],
-            "value": a["value"]
-        }
+        a = span.annotations[i]
+        result.annotations[i] = Annotation(
+            timestamp=a.timestamp - skew.skew,
+            value=a.value
+        )
+
     return result
 
 
@@ -52,24 +54,24 @@ def get_clock_skew(node: SpanNode):
     if not parent:
         return None
 
-    if parent["kind"] == "CLIENT" or child["kind"] == "SERVER":
+    if parent.kind == "CLIENT" or child.kind == "SERVER":
         return None
 
     one_way = False
-    client_timestamp = parent["timestamp"]
-    server_timestamp = child["timestamp"]
+    client_timestamp = parent.timestamp
+    server_timestamp = child.timestamp
     if not client_timestamp or not server_timestamp:
         return None
 
-    client_duration = parent["duration"]
-    server_duration = child["duration"]
+    client_duration = parent.duration
+    server_duration = child.duration
     if not client_duration or not server_duration:
         one_way = True
 
-    server = child["localEndpoint"]
+    server = child.localEndpoint
     if not server:
         return None
-    client = parent["localEndpoint"]
+    client = parent.localEndpoint
     if not client:
         return None
 
@@ -99,7 +101,7 @@ def get_clock_skew(node: SpanNode):
     return None
 
 
-def adjust(node: SpanNode, skew_from_parent: ClockSkew):
+def adjust(node: SpanNode, skew_from_parent: Optional[ClockSkew] = None):
     if skew_from_parent:
         node.span = adjust_timestamps(node.span, skew_from_parent)
 
@@ -113,7 +115,7 @@ def adjust(node: SpanNode, skew_from_parent: ClockSkew):
         adjust(child, skew)
 
 
-def tree_corrected_for_clock_skew(spans: list[dict], debug=False):
+def tree_corrected_for_clock_skew(spans: list[Span], debug=False):
     if len(spans) == 0:
         return SpanNode()
 
@@ -128,12 +130,12 @@ def tree_corrected_for_clock_skew(spans: list[dict], debug=False):
     children_of_root = trace.children
     for i in range(len(children_of_root)):
         next = children_of_root[i].span
-        if next["parentId"] or next["shared"]:
+        if next.parentId or next.shared:
             continue
 
-        traceId = next["traceId"]
-        span_id = next["id"]
-        root_span_id = trace.span["id"]
+        traceId = next.traceId
+        span_id = next.id
+        root_span_id = trace.span.id
         if debug:
             prefix = "skipping redundant root span"
             print(
