950d2f7e759f06361431c85f12432dfd1d962c95
Rafi Abbel <masterraf21@gmail.com>
2022-04-25 17:38:59 +0700

    async-ize jobs

--- src/scheduling/jobs.py
+++ src/scheduling/jobs.py
@@ -2,8 +2,9 @@ import json
 import time
 from datetime import datetime
 
-from src.config import ALPHA, state
 from critical_path import compare_critical_path
+from src.config import ALPHA, state
+from src.storage.repository import StorageRepository
 from statistic.ks import ks_test_same_dist
 from storage import retrieve_critical_path, retrieve_durations, store_json
 from transform import extract_critical_path, extract_durations
@@ -11,96 +12,96 @@ from zipkin.helper import adjust_traces
 from zipkin.models import AdjustedTrace, TraceParam
 from zipkin.query import query_traces
 
-from .constants import *
+from .constants import TRACE_LIMIT, BASELINE_PROBE_TIME, REALTIME_CHECK_PERIOD
 
 
-def get_baseline_traces() -> list[AdjustedTrace]:
-    raw_traces = query_traces(TraceParam(
-        endTs=round(time.time() * 1000),
-        limit=TRACE_LIMIT,
-        lookback=BASELINE_PROBE_TIME*1000
-    ))
-    traces = adjust_traces(raw_traces)
-    return traces
+class EngineJobs:
+    def __init__(self, redis_repo: StorageRepository) -> None:
+        self._storage_repo = redis_repo
 
+    def get_baseline_traces(self) -> list[AdjustedTrace]:
+        raw_traces = query_traces(TraceParam(
+            endTs=round(time.time() * 1000),
+            limit=TRACE_LIMIT,
+            lookback=BASELINE_PROBE_TIME*1000
+        ))
+        traces = adjust_traces(raw_traces)
+        return traces
 
-def get_realtime_traces() -> list[AdjustedTrace]:
-    raw_traces = query_traces(TraceParam(
-        endTs=round(time.time() * 1000),
-        limit=TRACE_LIMIT,
-        lookback=REALTIME_CHECK_PERIOD*1000
-    ))
-    traces = adjust_traces(raw_traces)
-    return traces
+    def get_realtime_traces(self) -> list[AdjustedTrace]:
+        raw_traces = query_traces(TraceParam(
+            endTs=round(time.time() * 1000),
+            limit=TRACE_LIMIT,
+            lookback=REALTIME_CHECK_PERIOD*1000
+        ))
+        traces = adjust_traces(raw_traces)
+        return traces
 
+    async def retrieve_baseline_models(self):
+        '''Query baseline traces from zipkin, transform into models
+        , store in redis, change global state'''
 
-def retrieve_baseline_models():
-    '''Query baseline traces from zipkin, transform into models
-    , store in redis, change global state'''
+        traces = self.get_baseline_traces()
 
-    traces = get_baseline_traces()
+        durations = extract_durations(traces)
+        paths = extract_critical_path(traces)
+        paths_json = [p.dict() for p in paths]
 
-    durations = extract_durations(traces)
-    paths = extract_critical_path(traces)
-    paths_json = [p.dict() for p in paths]
+        curr_dt = datetime.now()
+        timestamp = int(round(curr_dt.timestamp()))
+        durations_key = f"baseline_durations-{timestamp}"
+        paths_key = f"baseline_critical_path-{timestamp}"
 
-    curr_dt = datetime.now()
-    timestamp = int(round(curr_dt.timestamp()))
-    durations_key = f"baseline_durations-{timestamp}"
-    paths_key = f"baseline_critical_path-{timestamp}"
+        await self._storage_repo.store_json(durations_key, json.dumps(durations))
+        await self._storage_repo.store_json(paths_key, json.dumps(paths_json))
 
-    store_json(durations_key, json.dumps(durations))
-    store_json(paths_key, json.dumps(paths_json))
+        state.baselineReady = True
+        state.baselineKey.duration = durations_key
+        state.baselineKey.criticalPath = paths_key
 
-    state.baselineReady = True
-    state.baselineKey.duration = durations_key
-    state.baselineKey.criticalPath = paths_key
+    async def check_regression(self) -> bool:
+        '''Check realtime regression by comparing realtime
+        durations to baseline durations'''
+        if not state.baselineReady:
+            return False
 
+        realtime_traces = self.get_realtime_traces()
+        realtime_durations = extract_durations(realtime_traces)
+        baseline_durations = retrieve_durations(state.baselineKey.duration)
 
-def check_regression() -> bool:
-    '''Check realtime regression by comparing realtime
-    durations to baseline durations'''
-    if not state.baselineReady:
-        return False
+        same_distribution = ks_test_same_dist(
+            realtime_durations, baseline_durations, ALPHA)
 
-    realtime_traces = get_realtime_traces()
-    realtime_durations = extract_durations(realtime_traces)
-    baseline_durations = retrieve_durations(state.baselineKey.duration)
+        state.lastRegressionCheck = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+        state.isRegression = not same_distribution
+        return not same_distribution
 
-    same_distribution = ks_test_same_dist(
-        realtime_durations, baseline_durations, ALPHA)
+    async def perform_analysis(self):
+        '''Perform Critical Path analysis + Correlation Analysis
+        and store in redis'''
+        if not state.baselineReady:
+            return
 
-    state.lastRegressionCheck = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
-    state.isRegression = not same_distribution
-    return not same_distribution
+        realtime_traces = self.get_realtime_traces()
+        realtime_paths = extract_critical_path(realtime_traces)
+        baseline_paths = retrieve_critical_path(state.baselineKey.criticalPath)
 
+        result_paths = compare_critical_path(baseline_paths, realtime_paths)
+        result_paths_json = [p.json() for p in result_paths]
 
-def perform_analysis():
-    '''Perform Critical Path analysis + Correlation Analysis
-    and store in redis'''
-    if not state.baselineReady:
-        return
+        curr_dt = datetime.now()
+        timestamp = int(round(curr_dt.timestamp()))
+        result_paths_key = f"result_critical_path-{timestamp}"
 
-    realtime_traces = get_realtime_traces()
-    realtime_paths = extract_critical_path(realtime_traces)
-    baseline_paths = retrieve_critical_path(state.baselineKey.criticalPath)
+        await self._storage_repo.store_json(result_paths_key,
+                                            json.dumps(result_paths_json))
+        state.resultKey.criticalPath = result_paths_key
 
-    result_paths = compare_critical_path(baseline_paths, realtime_paths)
-    result_paths_json = [p.json() for p in result_paths]
-
-    curr_dt = datetime.now()
-    timestamp = int(round(curr_dt.timestamp()))
-    result_paths_key = f"result_critical_path-{timestamp}"
-
-    store_json(result_paths_key, json.dumps(result_paths_json))
-    state.resultKey.criticalPath = result_paths_key
-
-
-def regression_analysis(debug: bool = False):
-    if debug:
-        print("Doing Regression Analysis")
-    if check_regression():
-        perform_analysis()
+    async def regression_analysis(self, debug: bool = False):
+        if debug:
+            print("Doing Regression Analysis")
+        if self.check_regression():
+            await self.perform_analysis()
 
 
 def regression_analysis_fake():
