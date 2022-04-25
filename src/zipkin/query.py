bb4bfcc92d89fc4cda3869999be553485b6f4f2d
Rafi Abbel <masterraf21@gmail.com>
2022-04-25 21:16:20 +0700

    async-ize query

--- src/zipkin/query.py
+++ src/zipkin/query.py
@@ -1,20 +1,12 @@
-import requests
 import aiohttp
-from src.config import get_settings
-from .models import TraceParam, Span, DependencyLink
 from pydantic import parse_obj_as
+from src.config import get_settings
 from src.utils.checking import omit_none_dict
+
+from .models import DependencyLink, Span, TraceParam
 
 settings = get_settings()
 API = settings.zipkin_url
-
-
-def query_traces_sync(param: TraceParam) -> list[list[Span]]:
-    r = requests.get(url=f'{API}/traces', params=param.dict())
-    # print(r.url)
-    traces = r.json()
-    m = parse_obj_as(list[list[Span]], traces)
-    return m
 
 
 async def query_traces(param: TraceParam) -> list[list[Span]]:
@@ -26,41 +18,45 @@ async def query_traces(param: TraceParam) -> list[list[Span]]:
         return m
 
 
-def query_trace(id: str) -> list[Span]:
-    r = requests.get(url=f'{API}/trace/{id}')
-    spans = r.json()
-    m = parse_obj_as(list[Span], spans)
-    return m
+async def query_trace(id: str) -> list[Span]:
+    async with aiohttp.ClientSession() as session:
+        r = await session.get(url=f'{API}/trace/{id}')
+        spans = await r.json()
+        m = parse_obj_as(list[Span], spans)
+        return m
 
 
-def query_trace_many(ids: list[str]) -> list[list[Span]]:
-    ids_str = "".join(ids)
-    r = requests.get(url=f'{API}/traceMany', params={
-        "traceIds": ids_str
-    })
-    traces = r.json()
-    print(type(traces))
-    m = parse_obj_as(list[list[Span]], traces)
-    return m
+async def query_trace_many(ids: list[str]) -> list[list[Span]]:
+    async with aiohttp.ClientSession() as session:
+        ids_str = "".join(ids)
+        r = await session.get(url=f'{API}/traceMany', params={
+            "traceIds": ids_str
+        })
+        traces = await r.json()
+        m = parse_obj_as(list[list[Span]], traces)
+        return m
 
 
-def query_span_names(service_name: str) -> list[str]:
-    r = requests.get(url=f'{API}/spans', params={
-        "serviceName": service_name
-    })
-    return r.json()
+async def query_span_names(service_name: str) -> list[str]:
+    async with aiohttp.ClientSession() as session:
+        r = await session.get(url=f'{API}/spans', params={
+            "serviceName": service_name
+        })
+        return await r.json()
 
 
-def query_dependencies(end_ts: int, lookback: int = None) -> list[DependencyLink]:
-    r = requests.get(url=f'{API}/dependencies', params={
-        "endTs": end_ts,
-        "lookback": lookback
-    })
-    deps = r.json()
-    m = parse_obj_as(list[DependencyLink], deps)
-    return m
+async def query_dependencies(end_ts: int, lookback: int = None) -> list[DependencyLink]:
+    async with aiohttp.ClientSession() as session:
+        r = await session.get(url=f'{API}/dependencies', params={
+            "endTs": end_ts,
+            "lookback": lookback
+        })
+        deps = await r.json()
+        m = parse_obj_as(list[DependencyLink], deps)
+        return m
 
 
-def query_services() -> list[str]:
-    r = requests.get(url=f'{API}/services')
-    return r.json()
+async def query_services() -> list[str]:
+    async with aiohttp.ClientSession() as session:
+        r = await session.get(url=f'{API}/services')
+        return await r.json()
